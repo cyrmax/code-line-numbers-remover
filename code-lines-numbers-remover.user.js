bc90d0b329dccf473b969c09f7c04729456a3619
Cyrmax <belousov.k.m@yandex.ru>
2022-10-07 12:59:54 +0300

    Added update and download URLs

--- code-lines-numbers-remover.user.js
+++ code-lines-numbers-remover.user.js
@@ -1,9 +1,11 @@
 // ==UserScript==
 // @name         Metanit line remover
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Removes line numbers in code snippets on metanit
 // @author       Cyrmax
+// @updateURL https://cyrmax.github.io/code-line-numbers-remover
+// @downloadURL https://cyrmax.github.io/code-line-numbers-remover
 // @match        https://metanit.com/*
 // @run-at document-start
 // @grant unsafeWindow
